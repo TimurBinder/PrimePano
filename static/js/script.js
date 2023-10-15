91d2aa9ca0dfc623850b5dd2fab08be01593342f
TimurBinder <111724672+TimurBinder@users.noreply.github.com>
2023-10-15 21:10:19 +0500

    first block

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// Первый экран
+try {
+    document.addEventListener('DOMContentLoaded', () => {
+        // const title = document.querySelector('header .title');
+    
+        // title.querySelector('div:first-child').style.opacity = 1;
+    });
+} catch(e) {
+    console.error(e);
+}
